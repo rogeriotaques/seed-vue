061e18cf05d6c4de5a8cc7e7281f1e22cf185787
Rogerio Taques <rogerio.taques@gmail.com>
2020-12-17 23:43:23 +0900

    feat: added index

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as helpers from './utils/helpers';
+
+import SBadge from 'components/atoms/badge.vue';
+
+// import SAlert from 'components/alert.vue';
+// import SButton from 'components/button.vue';
+// import SSpinner from 'components/spinner.vue';
+// import SProgressBar from 'components/progress-bar.vue';
+// import SInput from 'components/input.vue';
+
+const version = '0.1.0';
+
+const components = { SBadge /*, SAlert, SButton, SSpinner, SProgressBar, SInput */ };
+
+const install: any = (Vue: any, config = {}): void => {
+  if (install.installed) {
+    return;
+  }
+
+  Object.keys(components).forEach((key: string): void => {
+    // eslint-disable-next-line
+    // @ts-ignore
+    const component = components[key];
+    Vue.component(component.name, component);
+  });
+};
+
+// Auto-install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+const SeedVue = { install, version, ...components, ...helpers };
+
+export default SeedVue;
+module.exports = SeedVue;
---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export enum COLORS_NAMES {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  RED = 'red',
+  ORANGE = 'orange',
+  GREEN = 'green',
+  BLUE = 'blue',
+  PURPLE = 'purple',
+  DARK = 'grey',
+  DARK_GREY = 'grey-dark',
+  SOFT_GREY = 'grey-light',
+  WHITE = 'white',
+  BLACK = 'black'
+}
+
+export enum SIZES_NAMES {
+  MINI = 'mini',
+  DEFAULT = 'default',
+  LARGE = 'large'
+}

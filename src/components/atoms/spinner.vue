0a2024f32974332d482daf6eba3966f3e7d88b8f
Rogerio Taques <rogerio.taques@gmail.com>
2020-12-22 22:02:49 +0900

    feat: added spinner

---
+++ src/components/atoms/spinner.vue
@@ -0,0 +1,106 @@
+<template lang="pug">
+  div(
+    :class="[`s-spinner--${color}`, `s-spinner--${size}`, `s-spinner--${speed}`]"
+  ).s-spinner
+    span(v-if="text").s-spinner__text {{ text }}
+</template>
+
+<script lang="ts">
+import { COLORS_NAMES, SIZES_NAMES } from 'utils/constants';
+
+export default {
+  name: 'SSpinner',
+
+  props: {
+    color: {
+      validator: (val) => val === null || Object.values(COLORS_NAMES).includes(val),
+      default: 'primary'
+    },
+
+    size: {
+      validator: (val) => null || Object.values(SIZES_NAMES).includes(val),
+      default: 'default'
+    },
+
+    speed: {
+      validator: (val) => val === null || ['default', 'fast', 'furious'].includes(val),
+      default: 'default'
+    },
+
+    text: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'styles/colors';
+@import 'styles/animations';
+@import 'styles/typography';
+@import 'styles/mixins';
+
+$s-spinner-gutter: 4px;
+$s-spinner-dimension: 24px;
+
+.s-spinner {
+  position: relative;
+  display: inline-block;
+  vertical-align: middle;
+  line-height: 30px;
+  margin: 10px;
+
+  &::before {
+    @include animation(spinner, 1.3s, infinite, linear);
+    content: '';
+    display: inline-block;
+    vertical-align: middle;
+    border: $s-spinner-gutter solid $c-border;
+    border-radius: 50%;
+    width: $s-spinner-dimension;
+    height: $s-spinner-dimension;
+  }
+
+  @each $colorName, $colorCode in $colors {
+    &--#{$colorName}::before {
+      @if $colorName == 'grey-light' {
+        border-color: #f9f9f9;
+      } @else if $colorName == 'white' {
+        border-color: $c-grey-dark;
+      }
+
+      border-top-color: $colorCode;
+      border-bottom-color: $colorCode;
+    }
+  }
+
+  &--mini::before {
+    border-width: $s-spinner-gutter / 3 * 2;
+    width: ($s-spinner-dimension / 3 * 2 - $s-spinner-gutter - ($s-spinner-gutter / 3 * 2));
+    height: ($s-spinner-dimension / 3 * 2 - $s-spinner-gutter - ($s-spinner-gutter / 3 * 2));
+  }
+
+  &--large::before {
+    border-width: $s-spinner-gutter * 2;
+    width: ($s-spinner-dimension * 2 - $s-spinner-gutter - ($s-spinner-gutter * 2));
+    height: ($s-spinner-dimension * 2 - $s-spinner-gutter - ($s-spinner-gutter * 2));
+  }
+
+  &--fast::before {
+    @include animation-duration(0.85s);
+  }
+
+  &--furious::before {
+    @include animation-duration(0.5s);
+  }
+
+  &__text {
+    @include margin-left(1);
+    display: inline-block;
+    vertical-align: middle;
+    font-size: $t-font-size-small;
+    color: $c-grey-dark;
+  } // .spinner:after
+}
+</style>
